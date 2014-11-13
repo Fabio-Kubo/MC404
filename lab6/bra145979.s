ad4f90d07fbcb760265a101d0c24f9e6942fba9e
Fabio Tadashi Kaneiwa Kubo <ra145979@zecolmeia.lab.ic.unicamp.br>
2014-11-13 14:36:48 -0200

    Lab 6

--- lab6/bra145979.s
+++ lab6/bra145979.s
@@ -5,7 +5,8 @@
 _start:                         @ main
         
         mov r0, #0              @ Carrega em r0 a velocidade do motor 0.
-                                @ Lembre-se: apenas os 6 bits menos significativos serao utilizados.
+                                @ Lembre-se: apenas os 6 bits menos significativos
+                                @ serao utilizados.
         mov r1, #0              @ Carrega em r1 a velocidade do motor 1.
         mov r7, #124            @ Identifica a syscall 124 (write_motors).
         svc 0x0                 @ Faz a chamada da syscall.
@@ -22,7 +23,9 @@ loop:
         mov r7, #125
         svc 0x0
 
+        mov r6, #0              @ Inicializa a variavel de controle de quem está mais perto do obstaculo (0-Sonar_3, 1-Sonar_4)
         cmp r5, r0              @ Compara o retorno (em r0) com r5.
+        movge r6, #1            @ Caso o sonar_4 esteja mais perto
         bge min                 @ Se r5 > r0: Salta pra min
         mov r0, r5              @ Senao: r0 <- r5
 
@@ -30,22 +33,37 @@ min:
         cmp r0, r6              @ Compara r0 com r6
         blt rotacionar          @ Se r0 menor que o limiar: Salta para rotacionar
 
-                                @ Senao define uma velocidade para os 2 motores
-        mov r0, #36            
+        mov r0, #36            @ Senao define uma velocidade para os 2 motores
         mov r1, #36
         mov r7, #124        
         svc 0x0
 
         b loop                  @ Refaz toda a logica
-        
 
-rotacionar:                            @ Rotaciona o robo
-        mov r0, #0              
-        mov r1, #18
+rotacionar:
+
+        cmp r6, #1
+        bge rotacionarDireita
+        b rotacionarEsquerda
+
+rotacionarEsquerda:             @ Rotaciona o Uóli para direita
+        
+        mov r0, #18  
+        mov r1, #0
         mov r7, #124
         svc 0x0
 
-        b loop
+        b loop                  @ Refaz toda a logica
+
+rotacionarDireita:              @ Rotaciona o Uóli para direita
+        
+        mov r0, #0 
+        mov r1, #18
+        mov r7, #124        
+        svc 0x0
+
+        b loop                  @ Refaz toda a logica
+
 
                 
 
