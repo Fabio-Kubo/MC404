36c9d93de33eb0ffcf5f6f1e64ebc11d23a75248
Fabio Tadashi Kaneiwa Kubo <ra145979@sabbath.lab.ic.unicamp.br>
2014-11-19 11:25:38 -0200

    Criação do lab 7

--- lab7/api_robot.s
+++ lab7/api_robot.s
@@ -7,37 +7,41 @@
 
 set_speed_motor:		@ Seta a velocidade do motor. Em r0 está a velocidade e em r1 está o motor id (0 for left motor, 1 for right motor)
 
-	stmfd SP!, {lr}
-	add r7, r0, #126 	@ Soma o id do motor com 126, pois 126 é o codigo para alterar o motor 0 e 127 para o motor 1
+	stmfd SP!, {r7, lr}
+	add r7, r1, #126 	@ Soma o id do motor com 126, pois 126 é o codigo para alterar o motor 0 e 127 para o motor 1
 	svc 0x0
-	ldmfd SP!, {lr}
+	ldmfd SP!, {r7, lr}
+	mov pc, lr
 
 set_speed_motors: 		@ Seta as velocidades dos motores. Em r0 está a velocidade do motor 0 e em r1 está a velocidade do motor 1
 
-	stmfd SP!, {lr}
+	stmfd SP!, {r7, lr}
 	mov r7, #124
 	svc 0x0
-	ldmfd SP!, {lr}
+	ldmfd SP!, {r7, lr}
+	mov pc, lr
 
 read_sonar: 			@ Le o sonar de acordo com o sonarId que é passado em r0. O retorno é em r0
 
-	stmfd SP!, {lr}
+	stmfd SP!, {r7, lr}
 	mov r7, #125
 	svc 0x0
-	ldmfd SP!, {lr}
+	ldmfd SP!, {r7, lr}
+	mov pc, lr
 
 read_sonars:
 
-	stmfd SP!, {lr}
-		mov r2, #0
-		ldr r3, [r0]
+	stmfd SP!, {r4, r5, lr}
+		mov r4, r0			@Copia o parametro para r4
+		mov r5, #0 			@Inicializa o contador
 	loop:
-		cmp r2, #15
-		bhi fim_loop
-		mov r0, r2
+		mov r0, r5		    @Seta o radarId
 		bl read_sonar
-		ldr r3, [r3, #4]	@Move o resultado da funcao para o vetor r3
-		add r2, r2, #1
+		str r0, [r4, #4]		@Move o resultado da funcao para o vetor r4
+		add r5, r5, #1
+		cmp r5, #15 		@Compara r5 com 15
+		bls loop
 	
 	fim_loop: 
-		ldmfd SP!, {lr}
+		ldmfd SP!, {r4, r5, lr}
+		mov pc, lr
